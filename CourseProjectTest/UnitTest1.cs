be5c3bcc53d72aeb01e128e90f40596a67ad98e7
Jimmy Kong <jimmyatwork368@gmail.com>
2025-03-21 14:25:47 -0700

    Add a new step in reusable.yml to install chrome and chromeDriver

---
+++ CourseProjectTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+public class SeleniumTests
+{
+    [Test]
+    public void OpenGoogleTest()
+    {
+        var options = new ChromeOptions();
+        options.AddArgument("--headless");
+        options.AddArgument("--no-sandbox");
+        options.AddArgument("--disable-dev-shm-usage");
+
+        string driverPath = Environment.GetEnvironmentVariable("CHROME_DRIVER_PATH");
+
+        using var driver = new ChromeDriver(driverPath, options);
+        driver.Navigate().GoToUrl("https://www.google.com");
+        StringAssert.Contains("Google", driver.Title);
+    }
+}
