a289886eb38e262de403156469a5dda1a6264787
tomhebcit <the18@my.bcit.ca>
2025-03-21 13:44:15 -0700

    updated models

--- CourseProject/Models/Service.cs
+++ CourseProject/Models/Service.cs
@@ -1,11 +1,10 @@
 namespace CourseProject.Models;
-
 public class Service
 {
     public int ServiceID { get; set; }
-    public String ServiceType { get; set; } 
-    public List<Employee> Employees { get; set; } = new List<Employee>(); 
+    public string Type { get; set; }
     public decimal Rate { get; set; }
     public List<string> Requirements { get; set; } = new List<string>();
+    public List<Employee> Employees { get; set; } = new List<Employee>();    // Navigation property for Employees associated with this service
 
 }
