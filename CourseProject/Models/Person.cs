c4c3198a0922d7fe9509a6618ea1becc955e6b11
tomhebcit <the18@my.bcit.ca>
2025-03-27 10:40:19 -0700

    switch the single Employee in EventSchedule to a list of Employees

--- CourseProject/Models/Person.cs
+++ CourseProject/Models/Person.cs
@@ -3,4 +3,5 @@ namespace CourseProject.Models;
 public class Person : RootObj
 {
     public string Name { get; set; }
+    public List<EventSchedule> EventSchedules { get; set; } = new List<EventSchedule>();
 }
