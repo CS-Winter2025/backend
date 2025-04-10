e25a60ed274b3c99510360e4c9dd14875602d96a
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-04-09 19:12:18 -0700

    Refactor detailsjson into dictionary

--- CourseProject/Controllers/ReportController.cs
+++ CourseProject/Controllers/ReportController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace CourseProject.Controllers
 {
@@ -15,11 +16,20 @@ namespace CourseProject.Controllers
         public async Task<IActionResult> Index()
         {
             var residents = await _context.Residents
-            .Include(r => r.EventSchedules)
-            .Include(r => r.Services)
-            .ToListAsync();
+                .Include(r => r.EventSchedules)
+                .Include(r => r.Services)
+                .ToListAsync();
+
+            ViewBag.ParsedDetails = ParseResidentDetails(residents);
 
             return View(residents);
+        }
+        private static Dictionary<int, Dictionary<string, object>> ParseResidentDetails(IEnumerable<Resident> residents)
+        {
+            return residents.ToDictionary(
+                r => r.ResidentId,
+                r => JsonConvert.DeserializeObject<Dictionary<string, object>>(r.DetailsJson ?? "{}") ?? []
+            );
         }
     }
 }
