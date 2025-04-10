008e3fbef32a99e829b350fbcf3c451942fd1331
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-04-10 00:17:00 -0700

    Add profile picture tag helper

---
+++ CourseProject/TagHelpers/ProfilePicture.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CourseProject.Helpers
+{
+    [HtmlTargetElement("profile-picture")]
+    public class ProfilePicture : TagHelper
+    {
+        public byte[] ImageData { get; set; }
+        public int MaxWidth { get; set; } = 100;
+        public int MaxHeight { get; set; } = 100;
+        public string Alt { get; set; } = "Profile Photo";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (ImageData != null && ImageData.Length > 0)
+            {
+                var base64 = Convert.ToBase64String(ImageData);
+                var src = $"data:image/jpeg;base64,{base64}";
+
+                output.TagName = "img";
+                output.Attributes.SetAttribute("src", src);
+                output.Attributes.SetAttribute("alt", Alt);
+                output.Attributes.SetAttribute("style", $"max-width: {MaxWidth}px; max-height: {MaxHeight}px;");
+            }
+            else
+            {
+                output.TagName = "span";
+                output.Content.SetContent("No photo");
+            }
+        }
+    }
+}
